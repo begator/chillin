34d03fa74fd0b6d959c24391b239cb48ab646ca2
begad mohamed <116667161+begator@users.noreply.github.com>
2024-10-01 05:35:39 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// إعداد الاتصال بقاعدة البيانات
+$servername = "j2ptqz3l.infinityfree.com";
+$username = "if0_37420788";
+$password = "Abosolimanbega7";
+$dbname = "if0_37420788_cillin";
+
+// إنشاء اتصال
+$conn = new mysqli($j2ptqz3l.infinityfree.com, $f0_37420788, $Abosolimanbega7, $if0_37420788_cillin);
+
+// التحقق من الاتصال
+if ($conn->connect_error) {
+    die("فشل الاتصال: " . $conn->connect_error);
+}
+?>
